d11a7e9e3c4d6347a99dea76247b7faa1803cb76
unknown <augustosantos1709@gmail.com>
2024-11-07 19:38:26 -0300

    New folder were add

---
+++ src/tipos-customizados.ts
@@ -0,0 +1,11 @@
+function movimentar(direcao: string, qtdPassos: number){
+    console.log(`O personagem andou ${qtdPassos} passos para ${direcao}`);
+
+}
+
+movimentar("cima", 2);
+movimentar("Cima", 4);
+movimentar("cimA", 6);
+movimentar("BAIXO", 2);
+movimentar("DireitA", 2);
+movimentar("abc", 2);
