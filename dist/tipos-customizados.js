311538e99c6a25fb287ebf9ca3cc84977349fe5d
unknown <augustosantos1709@gmail.com>
2024-11-07 19:40:29 -0300

    JavaScript is working

---
+++ dist/tipos-customizados.js
@@ -0,0 +1,10 @@
+"use strict";
+function movimentar(direcao, qtdPassos) {
+    console.log(`O personagem andou ${qtdPassos} passos para ${direcao}`);
+}
+movimentar("cima", 2);
+movimentar("Cima", 4);
+movimentar("cimA", 6);
+movimentar("BAIXO", 2);
+movimentar("DireitA", 2);
+movimentar("abc", 2);
