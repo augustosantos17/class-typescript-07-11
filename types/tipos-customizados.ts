65ab3a08cdbaee06d680cf13883834b67573a4bc
unknown <augustosantos1709@gmail.com>
2024-11-07 21:13:12 -0300

    movimentar.ts were moved to types

---
+++ types/tipos-customizados.ts
@@ -0,0 +1,15 @@
+
+
+type movimento = {
+    direcao: direcao;
+    qtdPassos: number;
+
+}
+
+function movimentar(move:movimento) {
+    console.log(`O personagem andou ${move.qtdPassos} passos para ${move.direcao}`);
+
+}
+
+movimentar({ direcao: "cima", qtdPassos: 2});
+
